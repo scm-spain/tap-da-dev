b2997aaf35389064e603ed5dd3d1d4baee286c2b
Roc Boronat <roc@fewlaps.com>
2016-02-19 12:23:34 +0100

    Finding the developers into the Activity and assigning them a OnClickListener that calls a method on the presenter

--- app/src/main/java/com/schibsted/tapdadev/MainActivity.java
+++ app/src/main/java/com/schibsted/tapdadev/MainActivity.java
@@ -1,13 +1,39 @@
 package com.schibsted.tapdadev;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    View developer1;
+    View developer2;
+    View developer3;
+
+    MainPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        presenter = new MainPresenter(this);
+
+        developer1 = findViewById(R.id.l_developer1).findViewById(R.id.iv_developer);
+        developer2 = findViewById(R.id.l_developer2).findViewById(R.id.iv_developer);
+        developer3 = findViewById(R.id.l_developer3).findViewById(R.id.iv_developer);
+
+        View.OnClickListener listener = new OnDeveloperClickListener();
+        developer1.setOnClickListener(listener);
+        developer2.setOnClickListener(listener);
+        developer3.setOnClickListener(listener);
+    }
+
+    public class OnDeveloperClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            presenter.hide(view);
+        }
     }
 }
---
+++ app/src/main/java/com/schibsted/tapdadev/MainPresenter.java
@@ -0,0 +1,16 @@
+package com.schibsted.tapdadev;
+
+import android.view.View;
+
+public class MainPresenter {
+
+    private final MainActivity activity;
+
+    public MainPresenter(MainActivity activity) {
+        this.activity = activity;
+    }
+
+    public void hide(View view) {
+
+    }
+}
