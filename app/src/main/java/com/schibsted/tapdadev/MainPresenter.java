d385b693931fca01b3a3f05ec9b8965fac215887
Roc Boronat <roc@fewlaps.com>
2016-02-19 13:19:25 +0100

    The developers come again after going away

--- app/src/main/java/com/schibsted/tapdadev/MainPresenter.java
+++ app/src/main/java/com/schibsted/tapdadev/MainPresenter.java
@@ -5,6 +5,8 @@ import android.util.Log;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 
+import java.util.Random;
+
 public class MainPresenter {
 
     public static final int GAME_ITERATION_DELAY = 50;
@@ -43,6 +45,21 @@ public class MainPresenter {
         view.setVisibility(View.INVISIBLE);
     }
 
+    private void show(int developer) {
+        if (developer == 1) {
+            show(developer1);
+        } else if (developer == 2) {
+            show(developer2);
+        } else if (developer == 3) {
+            show(developer3);
+        }
+    }
+
+    private void show(View view) {
+        view.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.slide_up));
+        view.setVisibility(View.VISIBLE);
+    }
+
     private void hide(final View view, int delay) {
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -57,11 +74,31 @@ public class MainPresenter {
             @Override
             public void run() {
                 if (gameStarted) {
-                    Log.d("GAME", "The game is on!");
-                    setNextGameIteration();
+                    runGameIteration();
                 }
             }
         }, GAME_ITERATION_DELAY);
+    }
+
+    private void runGameIteration() {
+        Log.d("GAME", "The game is on!");
+        if (allDevelopersAreHidden()) {
+            showOneRandomDeveloper();
+        }
+        setNextGameIteration();
+    }
+
+    private void showOneRandomDeveloper() {
+        Random random = new Random();
+        show(random.nextInt(3));
+    }
+
+    private boolean allDevelopersAreHidden() {
+        return isInvisible(developer1) && isInvisible(developer2) && isInvisible(developer3);
+    }
+
+    private boolean isInvisible(View view) {
+        return view.getVisibility() == View.INVISIBLE;
     }
 
     public void onPause() {
